a1f2df178c28d9f7c727a73a04d5d34f4ce73779
Gonzalo Coradello <gonzalocoradello@gmail.com>
2022-12-22 20:19:16 -0300

    first commit

---
+++ src/public/js/index.js
@@ -0,0 +1,42 @@
+let socket = io()
+let user = ''
+let chatBox = document.getElementById('chatbox')
+
+Swal.fire({
+    title: 'Authentication',
+    input: 'text',
+    text: 'Set username for the chat',
+    inputValidator: value => {
+        return !value.trim() && 'Please write a username'
+    },
+    allowOutsideClick: false
+}).then( result => {
+    user = result.value
+    document.getElementById('username').innerHTML = user
+   
+})
+
+chatBox.addEventListener('keyup', e => {
+    if(e.key === 'Enter') {
+        if(chatBox.value.trim().length > 0) {
+            socket.emit('message', {
+                user,
+                message: chatBox.value
+            })
+
+            chatBox.value = ''
+        }
+    }
+})
+
+// Recibir mensajes
+socket.on('logs', data => {
+    const logsDiv = document.getElementById('messageLogs')
+    let messages = ''
+
+    data.reverse().forEach(message => {
+        messages += `<p><i>${message.user}</i>: ${message.message}</p>`   
+    })
+
+    logsDiv.innerHTML = messages
+})
